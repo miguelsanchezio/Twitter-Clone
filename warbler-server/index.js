9b00f3afef391bbe9fd8e2bfc6a40b2b406935dd
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-04-03 13:07:41 -0700

    Created server setup.

---
+++ warbler-server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const PORT = 8081;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  let err = new Error('Not found.');
+  err.status = 404;
+  next(err);
+})
+
+// app.get('/', (req, res) => {
+//   res.send('Hello world.');
+// })
+
+app.listen(PORT, () => {
+  console.log(`Server is now running on ${PORT}.`);
+})
