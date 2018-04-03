a41579b0fced5252d5346689acafb80ea6e3a041
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-04-03 15:45:23 -0700

    Added ensureCorrectUser middleware.

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const errorHandler = require('./handlers/error');
 const authRoutes = require('./routes/auth');
 const messagesRoutes = require('./routes/messages');
+const { loginRequired, ensureCorrectUser } = require('./middleware/auth');
 
 const PORT = 8081;
 
@@ -13,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/users/:id/messages', messagesRoutes);
+app.use('/api/users/:id/messages', loginRequired, ensureCorrectUser, messagesRoutes);
 
 app.use((req, res, next) => {
   let err = new Error('Not found.');
--- warbler-server/middleware/auth.js
+++ warbler-server/middleware/auth.js
@@ -1,4 +1,4 @@
-require('dontenv').load();
+require('dotenv').load();
 const jwt = require('jsonwebtoken');
 
 exports.loginRequired = (req, res, next) => {
@@ -23,5 +23,22 @@ exports.loginRequired = (req, res, next) => {
 }
 
 exports.ensureCorrectUser = (req, res, next) => {
-
+  try {
+    const token = req.headers.authorization.split(' ')[1];   
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+      if(decoded && decoded.id === req.params.id) {
+        return next();
+      } else {
+        return next({
+          status: 401,
+          message: 'Unauthorized.'
+        });
+      }
+    })
+  } catch(err) {
+    return next({
+      status: 401,
+      message: 'Unauthorized.'
+    });
+  }
 }
