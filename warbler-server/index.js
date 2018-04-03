5e15daba9f8e95a5d69ed491b914392d0d574fd1
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-04-03 16:06:17 -0700

    Added additional message routes.

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -16,6 +16,20 @@ app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/users/:id/messages', loginRequired, ensureCorrectUser, messagesRoutes);
 
+app.get('/api/messages', loginRequired, async (req, res, next) => {
+  try {
+    let messages = await db.Message.find()
+      .sort({createdAt: 'desc'})
+      .populate('user', {
+        username: true,
+        profileImageUrl: true
+      });
+    return res.status(200).json(messages);
+  } catch(err) {
+    return next(err);
+  }
+});
+
 app.use((req, res, next) => {
   let err = new Error('Not found.');
   err.status = 404;
