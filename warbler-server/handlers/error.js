188761f8b66ca993849971eb4586689b564f1bfb
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-04-03 13:53:47 -0700

    Passwords are now encrypted and stored properly.

--- warbler-server/handlers/error.js
+++ warbler-server/handlers/error.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   return res.status(err.status || 500).json({
-    err: {
+    error: {
       message: err.message || 'There was an error.'
     }
   });
--- warbler-server/index.js
+++ warbler-server/index.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const errorHandler = require('./handlers/error');
+const authRoutes = require('./routes/auth');
 
 const PORT = 8081;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/api/auth', authRoutes);
 
 app.use((req, res, next) => {
   let err = new Error('Not found.');
@@ -16,10 +20,6 @@ app.use((req, res, next) => {
 })
 
 app.use(errorHandler);
-
-// app.get('/', (req, res) => {
-//   res.send('Hello world.');
-// })
 
 app.listen(PORT, () => {
   console.log(`Server is now running on ${PORT}.`);
