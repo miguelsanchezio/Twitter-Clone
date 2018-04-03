3399642baebb3255eca56f2aedbd7cdefd71f34a
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-04-03 15:06:11 -0700

    Added loginRequired middleware.

---
+++ warbler-server/middleware/auth.js
@@ -0,0 +1,27 @@
+require('dontenv').load();
+const jwt = require('jsonwebtoken');
+
+exports.loginRequired = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];   
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+      if(decoded) {
+        return next();
+      } else {
+        return next({
+          status: 401,
+          message: 'Log in first.'
+        });
+      }
+    })
+  } catch(err) {
+    return next({
+      status: 401,
+      message: 'Log in first.'
+    });
+  }
+}
+
+exports.ensureCorrectUser = (req, res, next) => {
+
+}
