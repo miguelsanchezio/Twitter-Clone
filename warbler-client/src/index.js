1e20a7fddc1db0f13ebeb073b7ebff55bfe354cb
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-04-03 17:29:16 -0700

    Set up redux store.

--- warbler-client/src/index.js
+++ warbler-client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import rootReducer from './store/reducers';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
